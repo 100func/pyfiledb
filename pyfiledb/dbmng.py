94c32027653d2ef1dd16cf93c4bac05579f75c93
I.Tanaka <I.Tanaka>
2022-07-10 17:00:12 +0900

    feat : dbmng.py基本計完成 検索、追加、テーブル作成

--- pyfiledb/dbmng.py
+++ pyfiledb/dbmng.py
@@ -1,8 +1,16 @@
+import pprint
 import sqlite3
+from typing import List
 
 
 DBNAME = 'hash.db'
 TABLE = 'hashtable'
+
+
+def hashpasser(data):
+    tmp = data.split('#')
+    del tmp[0]
+    return tmp
 
 
 class DbManager:
@@ -10,19 +18,38 @@ class DbManager:
         self.con = sqlite3.connect(dbname)
         self.cur = self.con.cursor()
 
-    def create_table(self) -> None:
+    def hastable(self):
+        tables = self.tables()
+        if len(tables) >= 1:
+            return TABLE in tables[0][1]
+        return False
+
+    def createtable(self) -> None:
         self.cur.execute(
-            f'create table {TABLE}(id integer primary key autoincrement, path text, hash text)',
+            f'create table if not exists {TABLE}(id integer primary key autoincrement, path text, hash text)',
         )
 
-    def append(self, path: str, hashs: list) -> None:
+    def append(self, path: str, hashs: str) -> None:
+        if isinstance(hashs, str):
+            hash_str = hashs
         self.cur.execute(
             f'insert into {TABLE} (path, hash) values (?, ?)',
-            (path, '#'.join(hashs), ),
+            (path, hash_str),
         )
 
-    def search(self, hashs: list):
-        pass
+    def allselect(self):
+        return self.cur.execute(f'select * from {TABLE}')
+
+    def search(self, hashs: List[str]) -> list:
+        tmp = []
+        print('hello')
+        for datum in self.allselect():
+            print('datum', datum)
+            for hash in hashs:
+                if hash in datum[-1]:
+                    print('hash', hash)
+                    tmp.append(datum[1])
+        return tmp
 
     def close(self) -> None:
         self.cur.close()
--- tests/test_dbmng.py
+++ tests/test_dbmng.py
@@ -1,6 +1,25 @@
-from pyfiledb.dbmng import DbManager
+import os
 
+from pyfiledb.dbmng import DbManager, DBNAME
 
+TEST_PATH = 'test/test'
+TEST_HASH = '#hash1#hash2'
 dbmng = DbManager()
-dbmng.create_table()
 
+
+def test_dbmng_create():
+    dbmng.createtable()
+    assert os.path.isfile(DBNAME)
+
+
+def test_dbmng_append():
+    dbmng.append(TEST_PATH, TEST_HASH)
+
+
+def test_dbmng_search():
+    reslut = dbmng.search(['hash7', 'hash1'])
+    assert TEST_PATH == reslut[0]
+
+
+def test_dbmng_close():
+    dbmng.close()
